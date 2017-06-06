fdfa11cd5472053545eae6ff0ebb82e2dfc85a81
Dante Frías <dante.frias@gmail.com>
2017-06-06 11:53:19 -0500

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,35 +1,122 @@
-# When done, submit this entire file to the autograder.
-
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum(arr)
+  total = 0
+  
+  if arr.length == 0
+    return total
+  else
+    arr.each do |n|
+      total += n
+    end
+  end  
+  
+  return total
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum(arr)
+  if arr.length == 0
+    return arr.length
+  elsif arr.length == 1	
+    return arr[0]
+  else
+	  arr = arr.sort()
+	  return arr[arr.length-1] + arr[arr.length-2]
+  end
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n?(arr, n)
+  result = false
+  
+  if arr.length <= 1
+    return result
+  else
+    until result do
+      first = arr.shift
+      
+      if first == nil
+        break
+      elsif arr.any? { |number| first + number == n }
+        result = true
+        break
+      end
+    end
+		
+    return result
+  end  
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+	"Hello, " << name
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant?(s)
+	if (s =~ /\A(?=[^aeiou])(?=[a-z])/i) == 0 
+    return true
+  else
+    return false
+  end 
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+def binary_multiple_of_4?(s)
+	exponent = 0
+	number_dec = 0
+	
+	if s.length == 0
+	  return false
+	end
+	
+	s.each_char { |c|
+		if c != "0" and c!= "1" 
+			return false
+		end	
+	} 
+	
+  exponent = s.length - 1
+
+  s.each_char { |c| 
+    number_dec += (2 ** exponent) * c.to_i
+    exponent -= 1
+  }
+    
+  return (number_dec % 4) == 0
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+	# Define default getter methods, but not setter methods.  
+  attr_reader :isbn, :price
+	
+  # When someone tries to set a isbn, enforce rules about it.  
+  def isbn=(isbn)  
+		if isbn == nil or isbn.size == 0 
+      raise ArgumentError.new('ISBN empty is not allowed.')  
+		end 
+		
+		@isbn = isbn	
+  end  
+  
+  # When someone tries to set a price, enforce rules about it.  
+  def price=(price)  
+    if price <= 0
+      raise ArgumentError.new('Price less or equal to zero is not allowed.')  
+    end  
+		
+		@price = price
+  end
+
+	# Delegate to the setter methods instead of setting the instance  
+  # variables directly.  
+  def initialize(isbn, price)  
+    self.isbn = isbn  
+    self.price = price  
+  end
+
+	def price_as_string	
+		'$' << '%.2f' % price.to_s
+	end
 end
+
